51dd5ae222ba47bab2053c9d3001778d513c7141
RustyRin <ggk494@gmail.com>
2022-07-06 12:24:28 -0400

    remove standard packages

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import *
 
 setup(
     name="bandcamp_api",
-    version="0.1",
+    version="0.1.01",
     description="Obtains information from bandcamp.com",
     author="RustyRin",
     packages=['bandcamp_api'],
-    requires=["setuptools", 'json', 'requests', "bs4", "datetime", "time", "logging", "random", "demjson3"],
+    url="https://github.com/RustyRin/bandcamp-api/",
+    install_requires=["setuptools", "bs4", "demjson3"],
     zip_safe=False
     )
