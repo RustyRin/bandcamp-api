1e1afbce64edaaf5ddfdd87462c862fcf1855973
Rin <ggk494@gmail.com>
2023-07-18 15:49:34 -0400

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import *
 
 setup(
     name="bandcamp_api",
-    version="0.1.15",
+    version="0.2.02",
     description="Obtains information from bandcamp.com",
     author="RustyRin",
     packages=['bandcamp_api'],
